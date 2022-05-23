db827dd20225c694237a2968d5db899e832e807c
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2022-05-23 21:18:40 +0200

    build.gradle update, small amendments in Cart and UserController

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/v1/user")
 public class UserController {
